c4585be4f19976513b44192cdba6c8383ed050d7
Minh_Tam <0306201285@caothang.edu.vn>
2023-06-15 08:33:47 +0700

    xoá product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable{
+        'name',
+        'image',
+        'price'
+    }
     use HasFactory;
 }
