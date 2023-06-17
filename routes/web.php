16f062b6549acb79f9553ae20daf3b166baeb0a2
Minh_Tam <0306201285@caothang.edu.vn>
2023-06-17 23:08:53 +0700

    trang chủ

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('trangchu');
 // });
+//trang chủ
+Route::get('/index', function () {
+    return view('index');
+})->name('index');
 Route::get('/', function () {
     return view('index');
 })->name('index');
