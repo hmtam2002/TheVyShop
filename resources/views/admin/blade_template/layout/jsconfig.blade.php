7a72ab303e356abdc047eeb4917d08e37c22cb83
Minh_Tam <0306201285@caothang.edu.vn>
2023-06-20 22:09:24 +0700

    Thêm bớt phần template admin

---
+++ resources/views/admin/blade_template/layout/jsconfig.blade.php
@@ -0,0 +1,29 @@
+<!-- Js Config -->
+<script type="text/javascript">
+    var PHP_VERSION = parseFloat('<?= phpversion() ?>'.replaceAll('.', ','));
+    var CONFIG_BASE = '<?= $configBase ?>';
+    var TOKEN = '<?= TOKEN ?>';
+    var ADMIN = '<?= ADMIN ?>';
+    var ASSET = '<?= ASSET ?>';
+    var LINK_FILTER = '<?= !empty($linkFilter) ? $linkFilter : '' ?>';
+    var ID = <?= !empty($id) ? $id : 0 ?>;
+    var COM = '<?= !empty($com) ? $com : '' ?>';
+    var ACT = '<?= !empty($act) ? $act : '' ?>';
+    var TYPE = '<?= !empty($type) ? $type : '' ?>';
+    var HASH = '<?= $func->generateHash() ?>';
+    var ACTIVE_GALLERY = <?= !empty($flagGallery) && !empty($gallery) ? 'true' : 'false' ?>;
+    var BASE64_QUERY_STRING = '<?= base64_encode($_SERVER['QUERY_STRING']) ?>';
+    var LOGIN_PAGE = <?= empty($_SESSION[$loginAdmin]['active']) ? 'true' : 'false' ?>;
+    var MAX_DATE = '<?= date('Y/m/d', time()) ?>';
+    var CHARTS = <?= !empty($charts) ? json_encode($charts) : '{}' ?>;
+    var ADD_OR_EDIT_PERMISSIONS =
+        <?= !empty($com) && $com == 'user' && !empty($act) && in_array($act, ['add_permission_group', 'edit_permission_group']) ? 'true' : 'false' ?>;
+    var IMPORT_IMAGE_EXCELL =
+        <?= !empty($com) && $com == 'import' && !empty($config['import']['images']) ? 'true' : 'false' ?>;
+    var ORDER_ADVANCED_SEARCH =
+        <?= !empty($com) && $com == 'order' && !empty($config['order']['search']) ? 'true' : 'false' ?>;
+    var ORDER_MIN_TOTAL = <?= !empty($minTotal) ? $minTotal : 1 ?>;
+    var ORDER_MAX_TOTAL = <?= !empty($maxTotal) ? $maxTotal : 1 ?>;
+    var ORDER_PRICE_FROM = <?= !empty($price_from) ? $price_from : 1 ?>;
+    var ORDER_PRICE_TO = <?= !empty($price_to) ? $price_to : (!empty($maxTotal) ? $maxTotal : 1) ?>;
+</script>
--- resources/views/admin/templates/layout/css.php
+++ resources/views/admin/templates/layout/css.php
@@ -2,19 +2,19 @@
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
 <!-- Css Files -->
-<link href="admin_assets/fontawesome611/all.css" rel="stylesheet">
-<link href="admin_assets/css/animate.min.css" rel="stylesheet">
-<link href="admin_assets/confirm/confirm.css" rel="stylesheet">
-<link href="admin_assets/select2/select2.css" rel="stylesheet">
-<link href="admin_assets/sumoselect/sumoselect.css" rel="stylesheet">
-<link href="admin_assets/datetimepicker/jquery.datetimepicker.css" rel="stylesheet">
-<link href="admin_assets/daterangepicker/daterangepicker.css" rel="stylesheet">
-<link href="admin_assets/rangeSlider/ion.rangeSlider.css" rel="stylesheet">
-<link href="admin_assets/filer/jquery.filer.css" rel="stylesheet">
-<link href="admin_assets/filer/jquery.filer-dragdropbox-theme.css" rel="stylesheet">
-<link href="admin_assets/holdon/HoldOn.css" rel="stylesheet">
-<link href="admin_assets/holdon/HoldOn-style.css" rel="stylesheet">
-<link href="admin_assets/simplenotify/simple-notify.css" rel="stylesheet">
-<link href="admin_assets/comment/comment.css" rel="stylesheet">
-<link href="admin_assets/css/adminlte.css" rel="stylesheet">
-<link href="admin_assets/css/adminlte-style.css" rel="stylesheet">
+<link href="assets/fontawesome611/all.css" rel="stylesheet">
+<link href="assets/css/animate.min.css" rel="stylesheet">
+<link href="assets/confirm/confirm.css" rel="stylesheet">
+<link href="assets/select2/select2.css" rel="stylesheet">
+<link href="assets/sumoselect/sumoselect.css" rel="stylesheet">
+<link href="assets/datetimepicker/jquery.datetimepicker.css" rel="stylesheet">
+<link href="assets/daterangepicker/daterangepicker.css" rel="stylesheet">
+<link href="assets/rangeSlider/ion.rangeSlider.css" rel="stylesheet">
+<link href="assets/filer/jquery.filer.css" rel="stylesheet">
+<link href="assets/filer/jquery.filer-dragdropbox-theme.css" rel="stylesheet">
+<link href="assets/holdon/HoldOn.css" rel="stylesheet">
+<link href="assets/holdon/HoldOn-style.css" rel="stylesheet">
+<link href="assets/simplenotify/simple-notify.css" rel="stylesheet">
+<link href="assets/comment/comment.css" rel="stylesheet">
+<link href="assets/css/adminlte.css" rel="stylesheet">
+<link href="assets/css/adminlte-style.css" rel="stylesheet">
